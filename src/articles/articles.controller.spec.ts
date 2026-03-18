8fbe4431cda9df20b0888502880451d318be2eae
Tim <timkentdev@gmail.com>
2026-03-17 21:47:28 -0700

    add husky and lint-staged pkgs

--- src/articles/articles.controller.spec.ts
+++ src/articles/articles.controller.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './providers/articles.service';
-import request from 'supertest'
 
 describe('ArticlesController', () => {
   let controller: ArticlesController;
@@ -15,13 +14,13 @@ describe('ArticlesController', () => {
           provide: ArticlesService,
           useValue: {
             findAll: jest.fn(() => {
-              return [{ id: 1 }, { id: 2 }]
+              return [{ id: 1 }, { id: 2 }];
             }),
-            findOne: jest.fn(()=> {
-              return { id: 1 }
-            })
-          }
-        }
+            findOne: jest.fn(() => {
+              return { id: 1 };
+            }),
+          },
+        },
       ],
     }).compile();
     controller = module.get<ArticlesController>(ArticlesController);
@@ -32,27 +31,27 @@ describe('ArticlesController', () => {
     expect(controller).toBeDefined();
   });
 
-  describe('GET /articles route', ()=> {
-    it('should call findAll service method once', ()=> {
-      controller.findAll()
-      expect(service.findAll).toHaveBeenCalledTimes(1)
-    })
+  describe('GET /articles route', () => {
+    it('should call findAll service method once', () => {
+      controller.findAll();
+      expect(service.findAll).toHaveBeenCalledTimes(1);
+    });
 
-    it('should return an array of article objects', ()=> {
-      const response = controller.findAll()
-      expect(response).toEqual([{ id: 1 }, { id: 2 }])
-    })
-  })
+    it('should return an array of article objects', () => {
+      const response = controller.findAll();
+      expect(response).toEqual([{ id: 1 }, { id: 2 }]);
+    });
+  });
 
-  describe('GET /articles/:id route', ()=> {
-    it('should call findOne service method once', ()=> {
-      controller.findOne("1")
-      expect(service.findOne).toHaveBeenCalledTimes(1)
-    })
+  describe('GET /articles/:id route', () => {
+    it('should call findOne service method once', () => {
+      controller.findOne('1');
+      expect(service.findOne).toHaveBeenCalledTimes(1);
+    });
 
-    it('should return an article object with the correct id', ()=> {
-      const response = controller.findOne("1");
-      expect(response).toEqual({ id: 1 })
-    })
-  })
+    it('should return an article object with the correct id', () => {
+      const response = controller.findOne('1');
+      expect(response).toEqual({ id: 1 });
+    });
+  });
 });
