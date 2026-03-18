ec8f5424e469023b20ade23c0a0e250bf73a30a1
Tim <timkentdev@gmail.com>
2026-03-18 14:04:46 -0700

    add docs decorators to findOne dto and endpoint

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ArticlesService } from './providers/articles.service';
 import { FindOneParamDto } from 'src/articles/dto/find-one-param.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('articles')
 export class ArticlesController {
@@ -12,6 +13,21 @@ export class ArticlesController {
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Finds one article using the ID provided in the route param.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Fetches the requested resource.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Error if param is invalid.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Error if resource does not exist.',
+  })
   findOne(@Param() findOneParamsDto: FindOneParamDto) {
     return this.articlesService.findOne(findOneParamsDto.id);
   }
--- src/articles/dto/find-one-param.dto.ts
+++ src/articles/dto/find-one-param.dto.ts
@@ -1,7 +1,12 @@
 import { Type } from 'class-transformer';
 import { IsInt, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class FindOneParamDto {
+  @ApiProperty({
+    description: 'ID of the article.',
+    example: 1,
+  })
   @Type(() => Number)
   @IsInt()
   @Min(1)
