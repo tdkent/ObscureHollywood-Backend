5f458b8e41f2a58bbcb29d8baf0a1292d070071b
Tim <timkentdev@gmail.com>
2026-03-18 13:34:26 -0700

    add swagger config to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Obscure Hollywood API')
+    .setDescription(
+      'Documentation of endpoints in the Obscure Hollywood REST API.',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
