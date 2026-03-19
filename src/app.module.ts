97887f9a5dbb545b1950e8e3fedef118fa9dc9ae
Tim <timkentdev@gmail.com>
2026-03-19 12:00:36 -0700

    add sentry module and global config module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ArticlesModule } from './articles/articles.module';
+import { SentryModule } from '@sentry/nestjs/setup';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SentryModule.forRoot(),
+    ArticlesModule,
+  ],
 })
 export class AppModule {}
