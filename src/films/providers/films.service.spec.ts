136a2de87bb25ce90766f6bb86898d4f9b6ad089
Tim <timkentdev@gmail.com>
2026-04-09 16:04:00 -0700

    add Film service test coverage

--- src/films/providers/films.service.spec.ts
+++ src/films/providers/films.service.spec.ts
@@ -1,16 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilmsService } from 'src/films/providers/films.service';
-// import { Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Film } from 'src/films/entities/film.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
 describe('FilmsService', () => {
   let service: FilmsService;
-  // let repository: jest.Mocked<Repository<Film>>
+  let repository: jest.Mocked<
+    Pick<Repository<Partial<Film>>, 'find' | 'count'>
+  >;
 
-  const mockFilmRepository = {
-    findAll: jest.fn(),
+  const mockFilmRepository: jest.Mocked<
+    Pick<Repository<Film>, 'find' | 'count'>
+  > = {
+    find: jest.fn(),
+    count: jest.fn(),
   };
 
   const mockConfigService = {
@@ -33,7 +38,7 @@ describe('FilmsService', () => {
     }).compile();
 
     service = module.get<FilmsService>(FilmsService);
-    // repository = module.get(getRepositoryToken(Film));
+    repository = module.get(getRepositoryToken(Film));
   });
 
   afterEach(() => {
@@ -42,5 +47,35 @@ describe('FilmsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll() database function', () => {
+    it('should call repository.count() and repository.find()', async () => {
+      repository.find.mockResolvedValue([]);
+      await service.findAll({ limit: 25, page: 100, orderBy: 'nameAsc' });
+      expect(repository.count).toHaveBeenCalledTimes(1);
+      expect(repository.find).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return an array of Film items', async () => {
+      const films: Partial<Film>[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
+      repository.find.mockResolvedValue(films);
+      const result = await service.findAll({
+        limit: 10,
+        page: 1,
+        orderBy: 'nameAsc',
+      });
+      expect(result.data).toEqual(films);
+    });
+
+    it('should return an empty array if no data can be found', async () => {
+      repository.find.mockResolvedValue([]);
+      const result = await service.findAll({
+        limit: 25,
+        page: 100,
+        orderBy: 'nameAsc',
+      });
+      expect(result.data).toEqual([]);
+    });
   });
 });
