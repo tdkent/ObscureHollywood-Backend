b2896d3e674df60ba3d80da6bddecd07b98f13b1
Tim <timkentdev@gmail.com>
2026-04-09 17:40:01 -0700

    update Article entity with Film relation

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,1 +1,74 @@
-export class Article {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  // OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+// import { ArticleRelation } from "../ArticleRelation/ArticleRelation.entity.js";
+import { Film, Film as FilmType } from 'src/films/entities/film.entity';
+// import { Person, type Person as PersonType } from "../Person/Person.entity.js";
+
+export enum Category {
+  FEATURE = 'feature',
+  FILM = 'film',
+  PERSON = 'person',
+}
+
+@Entity()
+export class Article {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 64,
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'enum',
+    enum: Category,
+  })
+  category: Category;
+
+  @Column({
+    type: 'text',
+  })
+  htmlContent: string;
+
+  @Column({
+    type: 'text',
+  })
+  textContent: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToOne(() => Film, (film) => film.article)
+  film: FilmType;
+
+  // @OneToOne(
+  // 	() => Person,
+  // 	(person) => person.article,
+  // )
+  // person: PersonType;
+
+  // @OneToMany(
+  // 	() => ArticleRelation,
+  // 	(ar) => ar.article,
+  // )
+  // outgoingRelations: ArticleRelation[];
+
+  // @OneToMany(
+  // 	() => ArticleRelation,
+  // 	(ar) => ar.relatedArticle,
+  // )
+  // incomingRelations: ArticleRelation[];
+}
