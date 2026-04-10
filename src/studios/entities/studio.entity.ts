3144b65b3317164bb38f49cadc6a051c13fc5e54
Tim <timkentdev@gmail.com>
2026-04-09 17:58:34 -0700

    add Studio entity

---
+++ src/studios/entities/studio.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Film, type Film as FilmType } from 'src/films/entities/film.entity';
+
+@Entity()
+export class Studio {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 64,
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'varchar',
+    length: 64,
+    unique: true,
+  })
+  name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Film, (film) => film.studio, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  films: FilmType[];
+}
