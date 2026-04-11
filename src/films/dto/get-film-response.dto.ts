c9ddbe1a56de0b8332371c19dffb8f4fb3583f2f
Tim <timkentdev@gmail.com>
2026-04-10 21:06:29 -0700

    create response type dto for getFilm endpoint

---
+++ src/films/dto/get-film-response.dto.ts
@@ -0,0 +1,20 @@
+import { FilmResponseDto } from 'src/films/dto/film-response.dto';
+import { ArticleResponseDto } from 'src/articles/dto/article-response.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { StudioResponseDto } from 'src/studios/dtos/studio-response.dto';
+import { PersonFilmResponseDto } from 'src/persons/dto/person-film-response.dto';
+import { FilmTagResponseDto } from 'src/films/dto/film-tag-response.dto';
+
+export class GetFilmResponseDto extends FilmResponseDto {
+  @ApiProperty({ type: ArticleResponseDto })
+  article: ArticleResponseDto;
+
+  @ApiProperty({ type: StudioResponseDto })
+  studio: StudioResponseDto;
+
+  @ApiProperty({ type: [PersonFilmResponseDto] })
+  personFilms: PersonFilmResponseDto[];
+
+  @ApiProperty({ type: [FilmTagResponseDto] })
+  filmTags: FilmTagResponseDto[];
+}
