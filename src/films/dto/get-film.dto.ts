6c0a77fc7748869e77fc2ae56245b0413b344d04
Tim <timkentdev@gmail.com>
2026-04-10 21:09:55 -0700

    add api documentation for getFilm endpoint

--- src/films/dto/get-film.dto.ts
+++ src/films/dto/get-film.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class GetFilmDto {
+  @ApiProperty({
+    description: 'Film title slug',
+    example: 'the-americano-1916',
+  })
   @IsString()
   @MinLength(1)
   @MaxLength(64)
