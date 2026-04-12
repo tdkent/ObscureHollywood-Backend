25c093ca59a7403f39faa16e6b07841393bfc931
Tim <timkentdev@gmail.com>
2026-04-12 09:53:47 -0700

    get persons and pagination metadata

--- src/persons/persons.module.ts
+++ src/persons/persons.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PersonsService } from './persons.service';
 import { PersonsController } from './persons.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from 'src/persons/entities/person.entity';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
 
 @Module({
   controllers: [PersonsController],
-  providers: [PersonsService],
+  providers: [PersonsService, PaginationProvider],
+  imports: [TypeOrmModule.forFeature([Person])],
 })
 export class PersonsModule {}
--- src/persons/persons.service.ts
+++ src/persons/persons.service.ts
@@ -1,10 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
 import { GetPersonsDto } from 'src/persons/dto/get-persons.dto';
+import { Person } from 'src/persons/entities/person.entity';
+import { IsNull, Not, Repository } from 'typeorm';
 
 @Injectable()
 export class PersonsService {
-  findAll(reqQuery: GetPersonsDto) {
-    return reqQuery;
+  constructor(
+    /**
+     * Persons repository
+     */
+    @InjectRepository(Person)
+    private personsRepository: Repository<Person>,
+    /**
+     * Pagination provider
+     */
+    private paginationProvider: PaginationProvider,
+  ) {}
+  /**
+   * Send a list of persons with pagination and sorting.
+   */
+  public async findAll(reqQuery: GetPersonsDto) {
+    const { limit, orderBy, page } = reqQuery;
+
+    const persons = await this.personsRepository.find({
+      where: {
+        article: Not(IsNull()),
+      },
+      order:
+        orderBy === 'lastNameDesc'
+          ? { lastName: 'DESC', firstName: 'DESC' }
+          : orderBy === 'firstNameAsc'
+            ? { firstName: 'ASC', lastName: 'ASC' }
+            : orderBy === 'firstNameDesc'
+              ? { firstName: 'DESC', lastName: 'DESC' }
+              : { lastName: 'ASC', firstName: 'ASC' },
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+
+    const finalResponse =
+      await this.paginationProvider.createPaginationMetadata({
+        repository: this.personsRepository,
+        limit,
+        page,
+        data: persons,
+      });
+
+    return finalResponse;
   }
 
   findOne(id: number) {
