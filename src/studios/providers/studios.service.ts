1d3505c2bbb9e2b143af72935ee6bee5845813a6
Tim <timkentdev@gmail.com>
2026-04-12 22:00:18 -0700

    find studios with pagination metadata

--- src/studios/providers/studios.service.ts
+++ src/studios/providers/studios.service.ts
@@ -1,10 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
 import { GetStudiosDto } from 'src/studios/dto/get-studio.dto';
+import { Studio } from 'src/studios/entities/studio.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class StudiosService {
-  findAll(reqQuery: GetStudiosDto) {
-    return reqQuery;
+  constructor(
+    /**
+     * Studios repository
+     */
+    @InjectRepository(Studio)
+    private studiosRepository: Repository<Studio>,
+    /**
+     * Pagination provider
+     */
+    private paginationProvider: PaginationProvider,
+  ) {}
+  public async findAll(reqQuery: GetStudiosDto) {
+    const { limit, orderBy, page } = reqQuery;
+
+    const studios = await this.studiosRepository.find({
+      order: orderBy === 'nameAsc' ? { name: 'ASC' } : { name: 'DESC' },
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+
+    const finalResponse =
+      await this.paginationProvider.createPaginationMetadata({
+        repository: this.studiosRepository,
+        limit,
+        page,
+        data: studios,
+      });
+
+    return finalResponse;
   }
 
   findOne(id: number) {
--- src/studios/studios.module.ts
+++ src/studios/studios.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StudiosService } from './providers/studios.service';
 import { StudiosController } from './studios.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Studio } from 'src/studios/entities/studio.entity';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
 
 @Module({
   controllers: [StudiosController],
-  providers: [StudiosService],
+  providers: [StudiosService, PaginationProvider],
+  imports: [TypeOrmModule.forFeature([Studio])],
 })
 export class StudiosModule {}
