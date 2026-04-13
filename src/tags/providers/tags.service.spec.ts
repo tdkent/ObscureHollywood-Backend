eae41ac6dc134251ecdd30e4d33cd617b4e51a4e
Tim <timkentdev@gmail.com>
2026-04-13 16:35:18 -0700

    add service tests

--- src/tags/providers/tags.service.spec.ts
+++ src/tags/providers/tags.service.spec.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { Tag } from 'src/tags/entities/tag.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
+import { GetTagsDto } from 'src/tags/dto/get-tag.dto';
 
 describe('TagsService', () => {
   let service: TagsService;
@@ -42,11 +43,93 @@ describe('TagsService', () => {
   });
 
   afterEach(() => {
-    console.log(repository);
     jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    const query: GetTagsDto = { limit: 3, orderBy: 'nameAsc', page: 1 };
+
+    it('should call repository.find()', async () => {
+      repository.find.mockResolvedValue([]);
+
+      await service.findAll(query);
+
+      expect(repository.find).toHaveBeenCalledTimes(1);
+
+      expect(repository.find).toHaveBeenCalledWith(
+        expect.objectContaining({
+          take: query.limit,
+          skip: (query.page - 1) * 10,
+          order: { name: 'ASC', type: 'ASC' },
+        }),
+      );
+    });
+
+    it('should return studios and pagination metadata', async () => {
+      const studios: Partial<Tag>[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
+
+      repository.find.mockResolvedValue(studios);
+
+      mockPaginationProvider.createPaginationMetadata.mockReturnValueOnce({
+        data: studios,
+        links: {
+          current: '',
+          first: '',
+        },
+        meta: {
+          currentPage: 1,
+          totalItems: 35,
+        },
+      });
+
+      const result = await service.findAll(query);
+
+      expect(result.data).toEqual(studios);
+      expect(result.links.current).toBeDefined();
+      expect(result.links.first).toBeDefined();
+      expect(result.meta.currentPage).toBe(1);
+      expect(result.meta.totalItems).toEqual(35);
+    });
+
+    it('should return an empty array if no data can be found', async () => {
+      repository.find.mockResolvedValue([]);
+      mockPaginationProvider.createPaginationMetadata.mockReturnValueOnce({
+        data: [],
+        links: {},
+        meta: {},
+      });
+      const result = await service.findAll(query);
+      expect(result.data).toEqual([]);
+    });
+  });
+
+  describe('findOne', () => {
+    const params = { slug: 'decade-1930s' };
+    const mockTag = { id: 1 };
+
+    it('should call repository.findOne()', async () => {
+      repository.findOne.mockResolvedValue(mockTag);
+
+      await service.findOne(params.slug);
+
+      expect(repository.findOne).toHaveBeenCalledTimes(1);
+      expect(repository.findOne).toHaveBeenCalledWith(
+        expect.objectContaining({
+          where: { slug: params.slug },
+        }),
+      );
+    });
+
+    it('should return the tag', async () => {
+      repository.findOne.mockResolvedValue(mockTag);
+
+      const result = await service.findOne(params.slug);
+
+      expect(result).toEqual(mockTag);
+    });
   });
 });
