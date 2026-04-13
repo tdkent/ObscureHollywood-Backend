88a2ed7a4e357441ec915284315548f61c58b99c
Tim <timkentdev@gmail.com>
2026-04-13 14:01:11 -0700

    add mocks to test files

--- src/tags/providers/tags.service.spec.ts
+++ src/tags/providers/tags.service.spec.ts
@@ -1,15 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TagsService } from 'src/tags/providers/tags.service';
+import { Repository } from 'typeorm';
+import { Tag } from 'src/tags/entities/tag.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
 
 describe('TagsService', () => {
   let service: TagsService;
+  let repository: jest.Mocked<
+    Pick<Repository<Partial<Tag>>, 'find' | 'findOne'>
+  >;
+
+  const mockTagRepository: jest.Mocked<
+    Pick<Repository<Partial<Tag>>, 'find' | 'findOne'>
+  > = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+  };
+
+  const mockPaginationProvider = {
+    createPaginationMetadata: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TagsService],
+      providers: [
+        TagsService,
+        {
+          provide: getRepositoryToken(Tag),
+          useValue: mockTagRepository,
+        },
+        {
+          provide: PaginationProvider,
+          useValue: mockPaginationProvider,
+        },
+      ],
     }).compile();
 
     service = module.get<TagsService>(TagsService);
+    repository = module.get(getRepositoryToken(Tag));
+  });
+
+  afterEach(() => {
+    console.log(repository);
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
--- src/tags/tags.controller.spec.ts
+++ src/tags/tags.controller.spec.ts
@@ -4,17 +4,37 @@ import { TagsService } from 'src/tags/providers/tags.service';
 
 describe('TagsController', () => {
   let controller: TagsController;
+  let service: TagsService;
+
+  const mockTagsService = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+  };
 
   beforeEach(async () => {
+    mockTagsService.findAll.mockResolvedValue([]);
+    mockTagsService.findOne.mockResolvedValue(null);
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TagsController],
-      providers: [TagsService],
+      providers: [
+        {
+          provide: TagsService,
+          useValue: mockTagsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<TagsController>(TagsController);
+    service = module.get<TagsService>(TagsService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
+    console.log(service);
     expect(controller).toBeDefined();
   });
 });
