c909cb3626d7b0d4b2f9b987ea3739b985766e35
Tim <timkentdev@gmail.com>
2026-04-13 11:32:05 -0700

    add mocks to test files

--- src/studios/providers/studios.service.spec.ts
+++ src/studios/providers/studios.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudiosService } from './studios.service';
+import { Repository } from 'typeorm';
+import { Studio } from 'src/studios/entities/studio.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
 
 describe('StudiosService', () => {
   let service: StudiosService;
+  let repository: jest.Mocked<
+    Pick<Repository<Partial<Studio>>, 'find' | 'findOne'>
+  >;
+
+  const mockStudioRepository: jest.Mocked<
+    Pick<Repository<Partial<Studio>>, 'find' | 'findOne'>
+  > = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+  };
+
+  const mockPaginationProvider = {
+    createPaginationMetadata: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StudiosService],
+      providers: [
+        StudiosService,
+        {
+          provide: getRepositoryToken(Studio),
+          useValue: mockStudioRepository,
+        },
+        {
+          provide: PaginationProvider,
+          useValue: mockPaginationProvider,
+        },
+      ],
     }).compile();
 
     service = module.get<StudiosService>(StudiosService);
+    repository = module.get(getRepositoryToken(Studio));
   });
 
   it('should be defined', () => {
+    console.log(repository);
     expect(service).toBeDefined();
   });
 });
--- src/studios/studios.controller.spec.ts
+++ src/studios/studios.controller.spec.ts
@@ -4,17 +4,37 @@ import { StudiosService } from './providers/studios.service';
 
 describe('StudiosController', () => {
   let controller: StudiosController;
+  let service: StudiosService;
+
+  const mockStudiosService = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+  };
 
   beforeEach(async () => {
+    mockStudiosService.findAll.mockResolvedValue([]);
+    mockStudiosService.findOne.mockResolvedValue(null);
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudiosController],
-      providers: [StudiosService],
+      providers: [
+        {
+          provide: StudiosService,
+          useValue: mockStudiosService,
+        },
+      ],
     }).compile();
 
     controller = module.get<StudiosController>(StudiosController);
+    service = module.get<StudiosService>(StudiosService);
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
+    console.log(service);
     expect(controller).toBeDefined();
   });
 });
