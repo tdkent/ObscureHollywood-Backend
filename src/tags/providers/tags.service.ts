dad7ec2260f6eb29a5ccae2d6c7cb13b98c810d6
Tim <timkentdev@gmail.com>
2026-04-13 13:35:25 -0700

    get tags with pagination metadata

--- src/tags/providers/tags.service.ts
+++ src/tags/providers/tags.service.ts
@@ -1,10 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
 import { GetTagsDto } from 'src/tags/dto/get-tag.dto';
+import { Repository } from 'typeorm';
+import { Tag } from 'src/tags/entities/tag.entity';
 
 @Injectable()
 export class TagsService {
-  findAll(reqQuery: GetTagsDto) {
-    return reqQuery;
+  constructor(
+    /**
+     * Tags repository
+     */
+    @InjectRepository(Tag)
+    private tagsRepository: Repository<Tag>,
+    /**
+     * Pagination provider
+     */
+    private paginationProvider: PaginationProvider,
+  ) {}
+  public async findAll(reqQuery: GetTagsDto) {
+    const { limit, orderBy, page } = reqQuery;
+
+    const tags = await this.tagsRepository.find({
+      order:
+        orderBy === 'nameDesc'
+          ? { name: 'DESC', type: 'DESC' }
+          : orderBy === 'typeAsc'
+            ? { type: 'ASC', name: 'ASC' }
+            : orderBy === 'typeDesc'
+              ? { type: 'DESC', name: 'DESC' }
+              : { name: 'ASC', type: 'ASC' },
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+
+    const finalResponse =
+      await this.paginationProvider.createPaginationMetadata({
+        repository: this.tagsRepository,
+        limit,
+        page,
+        data: tags,
+      });
+
+    return finalResponse;
   }
 
   findOne(slug: string) {
--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
+import { Tag } from 'src/tags/entities/tag.entity';
 import { TagsService } from 'src/tags/providers/tags.service';
 import { TagsController } from 'src/tags/tags.controller';
 
 @Module({
   controllers: [TagsController],
-  providers: [TagsService],
+  providers: [TagsService, PaginationProvider],
+  imports: [TypeOrmModule.forFeature([Tag])],
 })
 export class TagsModule {}
