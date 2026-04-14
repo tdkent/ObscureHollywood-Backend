e7846278a65f993bed06a65ba0387f7cb8044826
Tim <timkentdev@gmail.com>
2026-04-13 21:27:21 -0700

    remove endpoints and remove redundant article resource files

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ArticlesService } from './providers/articles.service';
-import { ArticlesController } from './articles.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticleRelation } from 'src/articles/entities/article-relation.entity';
 
 @Module({
-  controllers: [ArticlesController],
-  providers: [ArticlesService],
   imports: [TypeOrmModule.forFeature([ArticleRelation])],
 })
 export class ArticlesModule {}
