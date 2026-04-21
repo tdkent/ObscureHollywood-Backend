c2cab27d3ac322e17e7c00b6116902534e441cfe
Tim <timkentdev@gmail.com>
2026-04-21 15:31:43 -0700

    remove redundant

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
 import { Tag } from 'src/tags/entities/tag.entity';
 import { TagsService } from 'src/tags/providers/tags.service';
 import { TagsController } from 'src/tags/tags.controller';
 
 @Module({
   controllers: [TagsController],
-  providers: [TagsService, PaginationProvider],
+  providers: [TagsService],
   imports: [TypeOrmModule.forFeature([Tag])],
 })
 export class TagsModule {}
