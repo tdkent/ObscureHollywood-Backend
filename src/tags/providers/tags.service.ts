f7f59f2b94426ddcfda8674cd00ffa5561925c3a
Tim <timkentdev@gmail.com>
2026-04-21 15:26:56 -0700

    remove pagination from tags response

--- src/tags/providers/tags.service.ts
+++ src/tags/providers/tags.service.ts
@@ -1,7 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
-import { GetTagsDto } from 'src/tags/dto/get-tag.dto';
 import { Repository } from 'typeorm';
 import { Tag } from 'src/tags/entities/tag.entity';
 
@@ -13,37 +11,13 @@ export class TagsService {
      */
     @InjectRepository(Tag)
     private tagsRepository: Repository<Tag>,
-    /**
-     * Pagination provider
-     */
-    private paginationProvider: PaginationProvider,
   ) {}
-  public async findAll(reqQuery: GetTagsDto) {
-    const { limit, orderBy, page } = reqQuery;
-
+  public async findAll() {
     const tags = await this.tagsRepository.find({
-      order:
-        orderBy === 'nameDesc'
-          ? { name: 'DESC', type: 'DESC' }
-          : orderBy === 'typeAsc'
-            ? { type: 'ASC', name: 'ASC' }
-            : orderBy === 'typeDesc'
-              ? { type: 'DESC', name: 'DESC' }
-              : { name: 'ASC', type: 'ASC' },
-      take: limit,
-      skip: (page - 1) * limit,
+      order: { type: 'ASC', name: 'ASC' },
     });
 
-    const finalResponse =
-      await this.paginationProvider.createPaginationMetadata({
-        repository: this.tagsRepository,
-        limit,
-        orderBy,
-        page,
-        data: tags,
-      });
-
-    return finalResponse;
+    return tags;
   }
 
   public async findOne(slug: string) {
