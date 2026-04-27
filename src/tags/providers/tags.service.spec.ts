6c51b88bedd3f60a612ff4c6029c123f331e62ac
Tim <timkentdev@gmail.com>
2026-04-27 11:37:45 -0700

    create controller and service tests

--- src/tags/providers/tags.service.spec.ts
+++ src/tags/providers/tags.service.spec.ts
@@ -3,21 +3,40 @@ import { TagsService } from 'src/tags/providers/tags.service';
 import { Repository } from 'typeorm';
 import { Tag } from 'src/tags/entities/tag.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { GetFilmsByTagDto } from 'src/tags/dto/get-tag.dto';
+import { Film } from 'src/films/entities/film.entity';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
 
 describe('TagsService', () => {
   let service: TagsService;
+
+  const mockPaginationProvider = {
+    createPaginationMetadata: jest.fn(),
+  };
+
   let repository: jest.Mocked<
     Pick<Repository<Partial<Tag>>, 'find' | 'findOne'>
   >;
-
+  let filmRepository: jest.Mocked<
+    Pick<Repository<Partial<Film>>, 'findAndCount'>
+  >;
   const mockTagRepository: jest.Mocked<
     Pick<Repository<Partial<Tag>>, 'find' | 'findOne'>
   > = {
     find: jest.fn(),
     findOne: jest.fn(),
   };
+  const mockFilmRepository: jest.Mocked<
+    Pick<Repository<Partial<Film>>, 'findAndCount'>
+  > = {
+    findAndCount: jest.fn(),
+  };
 
   beforeEach(async () => {
+    mockTagRepository.find.mockResolvedValue([]);
+    mockTagRepository.findOne.mockResolvedValue({ id: 1 });
+    mockFilmRepository.findAndCount.mockResolvedValue([[], 0]);
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         TagsService,
@@ -25,11 +44,20 @@ describe('TagsService', () => {
           provide: getRepositoryToken(Tag),
           useValue: mockTagRepository,
         },
+        {
+          provide: getRepositoryToken(Film),
+          useValue: mockFilmRepository,
+        },
+        {
+          provide: PaginationProvider,
+          useValue: mockPaginationProvider,
+        },
       ],
     }).compile();
 
     service = module.get<TagsService>(TagsService);
     repository = module.get(getRepositoryToken(Tag));
+    filmRepository = module.get(getRepositoryToken(Film));
   });
 
   afterEach(() => {
@@ -42,24 +70,11 @@ describe('TagsService', () => {
 
   describe('findAll', () => {
     it('should call repository.find()', async () => {
-      repository.find.mockResolvedValue([]);
-
       await service.findAll();
-
       expect(repository.find).toHaveBeenCalledTimes(1);
     });
 
-    it('should return tags and pagination metadata', async () => {
-      const tags: Partial<Tag>[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
-      repository.find.mockResolvedValue(tags);
-
-      const data = await service.findAll();
-
-      expect(data).toEqual(tags);
-    });
-
     it('should return an empty array if no data can be found', async () => {
-      repository.find.mockResolvedValue([]);
       const data = await service.findAll();
       expect(data).toEqual([]);
     });
@@ -67,11 +82,8 @@ describe('TagsService', () => {
 
   describe('findOne', () => {
     const params = { slug: 'decade-1930s' };
-    const mockTag = { id: 1 };
 
     it('should call repository.findOne()', async () => {
-      repository.findOne.mockResolvedValue(mockTag);
-
       await service.findOne(params.slug);
 
       expect(repository.findOne).toHaveBeenCalledTimes(1);
@@ -83,11 +95,64 @@ describe('TagsService', () => {
     });
 
     it('should return the tag', async () => {
-      repository.findOne.mockResolvedValue(mockTag);
-
       const result = await service.findOne(params.slug);
+      expect(result).toEqual({ id: 1 });
+    });
+  });
 
-      expect(result).toEqual(mockTag);
+  describe('findFilmsByTag', () => {
+    const params = { slug: 'decade-1930s' };
+    const query: GetFilmsByTagDto = { limit: 3, orderBy: 'nameAsc', page: 1 };
+
+    it('should call Film repository.find()', async () => {
+      await service.findFilmsByTag(params.slug, query);
+
+      expect(filmRepository.findAndCount).toHaveBeenCalledTimes(1);
+      expect(filmRepository.findAndCount).toHaveBeenCalledWith(
+        expect.objectContaining({
+          take: query.limit,
+          skip: (query.page - 1) * 10,
+          order: { name: 'ASC', releaseYear: 'ASC' },
+        }),
+      );
+    });
+
+    it('should return films and pagination metadata', async () => {
+      const films: Partial<Film>[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
+
+      mockFilmRepository.findAndCount.mockResolvedValue([films, 3]);
+
+      mockPaginationProvider.createPaginationMetadata.mockReturnValueOnce({
+        data: films,
+        links: {
+          current: '',
+          first: '',
+        },
+        meta: {
+          currentPage: 1,
+          totalItems: 3,
+        },
+      });
+
+      const result = await service.findFilmsByTag(params.slug, query);
+
+      expect(result.data).toEqual(films);
+      expect(result.links.current).toBeDefined();
+      expect(result.links.first).toBeDefined();
+      expect(result.meta.currentPage).toBe(1);
+      expect(result.meta.totalItems).toEqual(3);
+    });
+
+    it('should return an empty array if no data can be found', async () => {
+      mockPaginationProvider.createPaginationMetadata.mockReturnValueOnce({
+        data: [],
+        links: {},
+        meta: {},
+      });
+
+      const result = await service.findFilmsByTag(params.slug, query);
+
+      expect(result.data).toEqual([]);
     });
   });
 });
