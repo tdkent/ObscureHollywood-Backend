7bbbe6e63c6a87ab8c0140062e8a144049ce1ae6
Tim <timkentdev@gmail.com>
2026-05-23 15:33:24 -0700

    add findAll controller tests

--- src/quiz/quiz.controller.spec.ts
+++ src/quiz/quiz.controller.spec.ts
@@ -1,20 +1,99 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { QuizController } from './quiz.controller';
-import { QuizService } from './quiz.service';
+import { QuizController } from 'src/quiz/quiz.controller';
+import { QuizService } from 'src/quiz/quiz.service';
+import { GetQuizzesDto } from 'src/quiz/dto/get-quizzes.dto';
+import { PaginatedResponse } from 'src/common/pagination/interfaces/paginated-response.interface';
+import { Quiz } from 'src/quiz/entities/quiz.entity';
 
 describe('QuizController', () => {
   let controller: QuizController;
+  let service: QuizService;
+
+  const mockQuizService = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+  };
 
   beforeEach(async () => {
+    mockQuizService.findAll.mockResolvedValue([]);
+    mockQuizService.findOne.mockResolvedValue(null);
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [QuizController],
-      providers: [QuizService],
+      providers: [
+        {
+          provide: QuizService,
+          useValue: mockQuizService,
+        },
+      ],
     }).compile();
 
     controller = module.get<QuizController>(QuizController);
+    service = module.get<QuizService>(QuizService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('findAll', () => {
+    const query: GetQuizzesDto = {
+      limit: '25',
+      page: '1',
+      orderBy: 'nameAsc',
+    };
+
+    it('should call findAll service once', async () => {
+      await service.findAll(query);
+      expect(service.findAll).toHaveBeenCalledTimes(1);
+    });
+
+    it('should pass query params to findAll service', async () => {
+      await service.findAll(query);
+      expect(service.findAll).toHaveBeenCalledWith(query);
+    });
+
+    it('should return an array of quizzes', async () => {
+      const quizzes = [{ id: 1 }, { id: 2 }, { id: 3 }];
+      const response: Partial<PaginatedResponse<Partial<Quiz>>> = {
+        data: quizzes,
+      };
+      mockQuizService.findAll.mockResolvedValue(response);
+      const result = await service.findAll(query);
+      expect(result.data).toBe(quizzes);
+    });
+
+    it('should return an empty array if no data can be found', async () => {
+      const response: Partial<PaginatedResponse<Partial<Quiz>>> = {
+        data: [],
+      };
+      mockQuizService.findAll.mockResolvedValue(response);
+      const result = await service.findAll(query);
+      expect(result.data).toEqual([]);
+    });
+  });
+
+  // describe('findOne', () => {
+  //   const slug = 'at-the-movies';
+  //   it('should call findOne service once', async () => {
+  //     await service.findOne(slug);
+  //     expect(service.findOne).toHaveBeenCalledTimes(1);
+  //   });
+
+  //   it('should pass the slug to the findOne service', async () => {
+  //     await service.findOne(slug);
+  //     expect(service.findOne).toHaveBeenCalledWith(slug);
+  //   });
+
+  //   it('should return the feature', async () => {
+  //     const feature = { id: 1 };
+  //     mockQuizService.findOne.mockResolvedValue(feature);
+  //     const result = await service.findOne(slug);
+  //     expect(result).toBe(feature);
+  //   });
+  // });
 });
