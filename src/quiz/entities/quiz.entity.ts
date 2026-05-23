ad430aca9cbf78c349d3993a3fbbc159f2b372f6
Tim <timkentdev@gmail.com>
2026-05-23 12:11:04 -0700

    create quiz, quiz question entities

--- src/quiz/entities/quiz.entity.ts
+++ src/quiz/entities/quiz.entity.ts
@@ -1,1 +1,50 @@
-export class Quiz {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import type { QuizQuestion } from 'src/quiz/entities/quiz-question.entity';
+
+export enum Theme {
+  FILMS = 'films',
+  GENRES = 'genres',
+  PEOPLE = 'people',
+}
+
+@Entity()
+export class Quiz {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 64,
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'varchar',
+    length: 64,
+    unique: true,
+  })
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: Theme,
+  })
+  theme: Theme;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @JoinColumn()
+  quizQuestions: QuizQuestion[];
+}
