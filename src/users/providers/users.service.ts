c4e05d24482211d7b0fa110a6c682daa31feb735
Tim <timkentdev@gmail.com>
2026-05-27 20:24:31 -0700

    get quiz results by user

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QuizResult } from 'src/quiz/entities/quiz-result.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    /** Quiz Result repository */
+    @InjectRepository(QuizResult)
+    private readonly quizResultRepository: Repository<QuizResult>,
+  ) {}
+
   public findById(userId: string) {
-    return userId;
+    return this.quizResultRepository.find({
+      where: { userId },
+      relations: { quiz: true },
+    });
   }
 
   public findQuizResultsById(userId: string, quizSlug: string) {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { QuizResult } from 'src/quiz/entities/quiz-result.entity';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([QuizResult])],
 })
 export class UsersModule {}
