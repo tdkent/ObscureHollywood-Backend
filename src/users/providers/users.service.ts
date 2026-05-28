e6f117c8b8f44752a2ea6f4c126655fbcb768102
Tim <timkentdev@gmail.com>
2026-05-28 11:55:19 -0700

    create querybuilder to get quiz count, average score, etc, for all quizzes

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QuizResult } from 'src/quiz/entities/quiz-result.entity';
+import { Quiz } from 'src/quiz/entities/quiz.entity';
 import { Repository } from 'typeorm';
+
+interface QuizResults {
+  totalCount: number;
+  distinctCount: number;
+  avgScore: number | null;
+  quizCount: number;
+}
 
 @Injectable()
 export class UsersService {
@@ -11,14 +19,32 @@ export class UsersService {
     private readonly quizResultRepository: Repository<QuizResult>,
   ) {}
 
-  public findUserQuizResults(userId: string) {
-    return this.quizResultRepository.find({
-      where: { userId },
-      relations: { quiz: true },
-    });
+  /** Get count of quizzes, attempts, average score for all quizzes.  */
+  public async findUserQuizResults(userId: string) {
+    const result = (await this.quizResultRepository
+      .createQueryBuilder('quizResult')
+      .where('quizResult.userId = :userId', { userId })
+      .select('CAST(COUNT(*) AS INT)', 'totalCount')
+      .addSelect(
+        'CAST(COUNT(DISTINCT quizResult.quiz) AS INT)',
+        'distinctCount',
+      )
+      .addSelect('CAST(ROUND(AVG(quizResult.score), 1) AS DECIMAL)', 'avgScore')
+      .addSelect((subQuery) => {
+        return subQuery
+          .select('CAST(COUNT(*) AS INT)', 'quizCount')
+          .from(Quiz, 'quiz');
+      })
+      .getRawOne()) as QuizResults;
+
+    const percentComplete = Math.round(
+      Number((result.distinctCount / result.quizCount) * 100),
+    );
+
+    return { ...result, avgScore: Number(result.avgScore), percentComplete };
   }
 
-  /** Get count of attempts, high score, and most recent score. */
+  /** Get count of attempts, high score, and most recent score for a single quiz. */
   public async findUserSingleQuizResults(userId: string, quizSlug: string) {
     return this.quizResultRepository
       .createQueryBuilder('quizResult')
