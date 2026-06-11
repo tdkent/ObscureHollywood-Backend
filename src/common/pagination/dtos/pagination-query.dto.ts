9ac9dab300529be1f884b36615a7ba2d1a3d21b8
Tim <timkentdev@gmail.com>
2026-06-11 15:07:03 -0700

    remove limit prop from pagination dto

--- src/common/pagination/dtos/pagination-query.dto.ts
+++ src/common/pagination/dtos/pagination-query.dto.ts
@@ -3,13 +3,6 @@ import { IsString } from 'class-validator';
 
 export class PaginationQueryDto {
   @ApiProperty({
-    description: 'Maximum results to return per page.',
-    example: '25',
-  })
-  @IsString()
-  limit: string = '25';
-
-  @ApiProperty({
     description: 'Pagination page.',
     example: '1',
   })
